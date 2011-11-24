8fd44d273b05473c4d3326d9c2752df7fa071f3b
Chris Stringer <chris.stringer@g5searchmarketing.com>
2011-11-23 20:50:55 -0800

    Modify SimpleWorker config for v2 support

--- config/initializers/simple_worker.rb
+++ config/initializers/simple_worker.rb
@@ -1,6 +1,6 @@
 SimpleWorker.configure do |config|
-    config.access_key = ENV['SIMPLE_WORKER_ACCESS_KEY']
-    config.secret_key = ENV['SIMPLE_WORKER_SECRET_KEY']
+    config.token = ENV['SIMPLE_WORKER_TOKEN']
+    config.project_id = ENV['SIMPLE_WORKER_PROJECT_ID']
     # Use the line below if you're using an ActiveRecord database
     #config.database = Rails.configuration.database_configuration[Rails.env]
 end
